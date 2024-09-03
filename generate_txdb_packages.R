ee65d6b9e924ee10b01c9c686de291bc9f728c2f
Pål O. Westermark <westermark@fbn-dummerstorf.de>
2024-09-03 17:10:11 +0200

    Code to generate more packages

--- generate_txdb_packages.R
+++ generate_txdb_packages.R
@@ -2,9 +2,6 @@
 # Note that locally, RMariaDB works with mariadb-connector-c version 3.3.8,
 # but not with 3.4.1.
 library(txdbmaker)
-library(msigdbr)
-library(GSEABase)
-library(rjson)
 library(tidyverse)
 library(pwrutilities)
 
@@ -48,4 +45,69 @@ makeTxDbPackageFromUCSC(
 )
 
 
+makeTxDbPackageFromUCSC(
+  version=txdbversion,
+  genome="mm39",
+  maintainer=as.character(pw),
+  author=as.character(pw),
+  tablename="ncbiRefSeqCurated",
+  circ_seqs = "chrM",
+  miRBaseBuild="GRCm39"
+)
 
+makeTxDbPackageFromUCSC(
+  version=txdbversion,
+  genome="mm10",
+  maintainer=as.character(pw),
+  author=as.character(pw),
+  tablename="ncbiRefSeq",
+  circ_seqs = "chrM",
+  miRBaseBuild="GRCm38"
+)
+
+makeTxDbPackageFromUCSC(
+  version=txdbversion,
+  genome="mm10",
+  maintainer=as.character(pw),
+  author=as.character(pw),
+  tablename="refGene",
+  circ_seqs = "chrM",
+  miRBaseBuild="GRCm38"
+)
+
+makeTxDbPackageFromUCSC(
+  version=txdbversion,
+  genome="mm10",
+  maintainer=as.character(pw),
+  author=as.character(pw),
+  tablename="knownGene",
+  circ_seqs = "chrM",
+  miRBaseBuild="GRCm38"
+)
+
+
+makeTxDbPackageFromBiomart(
+  version = txdbversion,
+  dataset = "mmusculus_gene_ensembl",
+  maintainer = as.character(pw),
+  author = as.character(pw),
+  circ_seqs = "MT"
+)
+
+makeTxDbPackageFromUCSC(
+  version=txdbversion,
+  genome="dm3",
+  maintainer=as.character(pw),
+  author=as.character(pw),
+  circ_seqs = "chrM",
+  tablename="refGene"
+)
+
+makeTxDbPackageFromUCSC(
+  version=txdbversion,
+  genome="dm3",
+  maintainer=as.character(pw),
+  author=as.character(pw),
+  circ_seqs = "chrM",
+  tablename="flyBaseGene"
+)
